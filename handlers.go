ebd74da6a4ffccb79f3cd3303c9daa644449dba9
Jerome Amon <aaboujerome@gmail.com>
2023-04-05 23:45:06 +0200

    refactor: move routes setup to router.go file

--- handlers.go
+++ handlers.go
@@ -32,21 +32,6 @@ func NewAPIHandler(logger *zap.Logger, config *Config, stats *Statistics, bs Boo
 	return &APIHandler{logger: logger, config: config, stats: stats, bookService: bs}
 }
 
-// SetupRoutes enforces the api routes.
-func (api *APIHandler) SetupRoutes(router *httprouter.Router) *httprouter.Router {
-	router.RedirectTrailingSlash = true
-	router.GET("/", api.middleware(api.Index))
-	router.GET("/status", api.middleware(api.Index))
-	// router.GET("/internal/stats", api.middleware(api.GetStats))
-	router.GET("/internal/configs", api.middleware(api.GetConfigs))
-	router.POST("/v1/books", api.middleware(api.CreateBook))
-	router.GET("/v1/books", api.middleware(api.GetAllBooks))
-	router.GET("/v1/books/:id", api.middleware(api.GetOneBook))
-	router.PUT("/v1/books/:id", api.middleware(api.UpdateBook))
-	router.DELETE("/v1/books/:id", api.middleware(api.DeleteOneBook))
-	return router
-}
-
 // setupCORS is a helper function which set cors headers.
 func setupCORS(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
