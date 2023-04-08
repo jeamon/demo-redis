3e802b11b8ecbe8d85604c9fce6c3c3f6351abe5
Jerome Amon <aaboujerome@gmail.com>
2023-04-08 22:21:26 +0200

    refactor: wraps handlers with passed middlewares

--- router.go
+++ router.go
@@ -3,16 +3,16 @@ package main
 import "github.com/julienschmidt/httprouter"
 
 // SetupRoutes enforces the api routes.
-func (api *APIHandler) SetupRoutes(router *httprouter.Router) *httprouter.Router {
+func (api *APIHandler) SetupRoutes(router *httprouter.Router, m Middlewares) *httprouter.Router {
 	router.RedirectTrailingSlash = true
-	router.GET("/", api.middleware(api.Index))
-	router.GET("/status", api.middleware(api.Index))
-	// router.GET("/ops/stats", api.middleware(api.GetStats))
-	router.GET("/ops/configs", api.middleware(api.GetConfigs))
-	router.POST("/v1/books", api.middleware(api.CreateBook))
-	router.GET("/v1/books", api.middleware(api.GetAllBooks))
-	router.GET("/v1/books/:id", api.middleware(api.GetOneBook))
-	router.PUT("/v1/books/:id", api.middleware(api.UpdateBook))
-	router.DELETE("/v1/books/:id", api.middleware(api.DeleteOneBook))
+	router.GET("/", m.Chain(api.Index))
+	router.GET("/status", m.Chain(api.Index))
+	// router.GET("/ops/stats", api.GetStats)
+	router.GET("/ops/configs", m.Chain(api.GetConfigs))
+	router.POST("/v1/books", m.Chain(api.CreateBook))
+	router.GET("/v1/books", m.Chain(api.GetAllBooks))
+	router.GET("/v1/books/:id", m.Chain(api.GetOneBook))
+	router.PUT("/v1/books/:id", m.Chain(api.UpdateBook))
+	router.DELETE("/v1/books/:id", m.Chain(api.DeleteOneBook))
 	return router
 }
