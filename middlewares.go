ba2e25c41c2e2931a4e37b6a87e2036e97e302bb
Jerome Amon <aaboujerome@gmail.com>
2023-04-08 22:07:40 +0200

    refactor: offload panic recovery middleware and add middlewares chain function

--- middlewares.go
+++ middlewares.go
@@ -10,23 +10,20 @@ import (
 	"go.uber.org/zap"
 )
 
-// middleware is used to intercept incoming HTTP calls then attach a unique id to each request
+// MiddlewareFunc is a custom type for ease of use.
+type MiddlewareFunc func(httprouter.Handle) httprouter.Handle
+
+// Middlewares is a custom type to represent a stack of
+// middleware functions used to build a single chain.
+type Middlewares []MiddlewareFunc
+
+// CoreMiddleware is used to intercept incoming HTTP calls then attach a unique id to each request
 // and log each request details and finally apply cors headers on it.
-func (api *APIHandler) middleware(next httprouter.Handle) httprouter.Handle {
+func (api *APIHandler) CoreMiddleware(next httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		atomic.AddUint64(&api.stats.called, 1)
 		start := time.Now()
 		requestID := GenerateRequestID()
-		defer func() {
-			if err := recover(); err != nil {
-				api.logger.Error("panic occurred", zap.String("requestid", requestID), zap.Any("error", err))
-				errResp := NewAPIError(requestID, http.StatusInternalServerError, "failed to process the request.", EmptyData)
-				if err := WriteErrorResponse(w, errResp); err != nil {
-					api.logger.Error("failed to send error response", zap.String("requestid", requestID), zap.Error(err))
-				}
-			}
-		}()
-
 		ctx, cancel := context.WithTimeout(r.Context(), time.Duration(api.config.Server.RequestTimeout)*time.Second)
 		defer cancel()
 		ctx = context.WithValue(ctx, ContextRequestID, requestID)
@@ -50,4 +47,39 @@ func (api *APIHandler) middleware(next httprouter.Handle) httprouter.Handle {
 			zap.Duration("duration", time.Since(start)),
 		)
 	}
+}
+
+// PanicRecoveryMiddleware catches any panic during the request lifecycle and produces
+// an error log for further analysis. It sends a failure response to the client with 500.
+func (api *APIHandler) PanicRecoveryMiddleware(next httprouter.Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		recovery := func() {
+			if err := recover(); err != nil {
+				requestID := r.Context().Value(ContextRequestID).(string)
+				api.logger.Error("panic occurred", zap.String("requestid", requestID), zap.Any("error", err))
+				errResp := NewAPIError(requestID, http.StatusInternalServerError, "failed to process the request.", EmptyData)
+				if err := WriteErrorResponse(w, errResp); err != nil {
+					api.logger.Error("failed to send error response", zap.String("requestid", requestID), zap.Error(err))
+				}
+			}
+		}
+		defer recovery()
+		next(w, r, ps)
+	}
+}
+
+// Chain wraps a given httprouter.Handle with a list of middlewares.
+// It does by starting from the last middleware from the list.
+func (m Middlewares) Chain(h httprouter.Handle) httprouter.Handle {
+	if len(m) == 0 {
+		return h
+	}
+	lg := len(m)
+	handle := m[lg-1](h)
+
+	for i := lg - 2; i >= 0; i-- {
+		handle = m[i](handle)
+	}
+
+	return handle
 }
