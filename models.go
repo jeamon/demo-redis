91fd20bdfd13a22604ae317ea8f261bd6dfcaa34
Jerome Amon <aaboujerome@gmail.com>
2023-04-16 00:18:34 +0200

    refactor: move non related content to another files

--- models.go
+++ models.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"net/http"
-)
-
 // Book represents a book entity.
 type Book struct {
 	ID          string `json:"id" binding:"required"`
@@ -14,54 +9,4 @@ type Book struct {
 	Price       string `json:"price" binding:"required"`
 	CreatedAt   string `json:"createdAt"`
 	UpdatedAt   string `json:"updatedAt"`
-}
-
-// APIError is the data model sent when an error occurred during request processing.
-type APIError struct {
-	RequestID string      `json:"requestid"`
-	Status    int         `json:"status"`
-	Message   string      `json:"message"`
-	Data      interface{} `json:"data"`
-}
-
-// APIResponse is the data model sent when a request succeed.
-// We use the omitempty flag on the `total` field. This helps
-// set the value for `GetAllBook` calls only.
-type APIResponse struct {
-	RequestID string      `json:"requestid"`
-	Status    int         `json:"status"`
-	Message   string      `json:"message"`
-	Total     *int        `json:"total,omitempty"`
-	Data      interface{} `json:"data"`
-}
-
-func NewAPIError(requestid string, status int, message string, data interface{}) *APIError {
-	return &APIError{
-		RequestID: requestid,
-		Status:    status,
-		Message:   message,
-		Data:      data,
-	}
-}
-
-func GenericResponse(requestid string, status int, message string, total *int, data interface{}) *APIResponse {
-	return &APIResponse{
-		RequestID: requestid,
-		Status:    status,
-		Message:   message,
-		Total:     total,
-		Data:      data,
-	}
-}
-
-func WriteErrorResponse(w http.ResponseWriter, errResp *APIError) error {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(errResp.Status)
-	return json.NewEncoder(w).Encode(errResp)
-}
-
-func WriteResponse(w http.ResponseWriter, resp *APIResponse) error {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(resp.Status)
-	return json.NewEncoder(w).Encode(resp)
 }
