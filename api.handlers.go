e16fed9f2ae3e201cc9d9a6d7a1bf5795c7dafa5
Jerome Amon <aaboujerome@gmail.com>
2023-04-19 22:59:07 +0200

    feat: add current maintenance mode details in statistics response

--- api.handlers.go
+++ api.handlers.go
@@ -125,11 +125,12 @@ func (api *APIHandler) GetStatistics(w http.ResponseWriter, r *http.Request, _ h
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	if err := json.NewEncoder(w).Encode(
 		map[string]interface{}{
-			"requestid": requestID,
-			"version":   api.stats.version,
-			"called":    atomic.LoadUint64(&api.stats.called),
-			"started":   api.stats.started.Format(time.RFC1123),
-			"uptime":    fmt.Sprintf("%.0f mins", time.Since(api.stats.started).Minutes()),
+			"requestid":   requestID,
+			"version":     api.stats.version,
+			"called":      atomic.LoadUint64(&api.stats.called),
+			"started":     api.stats.started.Format(time.RFC1123),
+			"uptime":      fmt.Sprintf("%.0f mins", time.Since(api.stats.started).Minutes()),
+			"maintenance": api.mode,
 		},
 	); err != nil {
 		api.logger.Error("failed to send statistics response", zap.String("request.id", requestID), zap.Error(err))
