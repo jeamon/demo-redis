332f98d7a4f23906c73313f96410250519b60d04
Jerome Amon <aaboujerome@gmail.com>
2023-06-03 13:27:06 +0200

    test: add more unit tests to BoltDB repository

--- tests.repo.bolt_test.go
+++ tests.repo.bolt_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"os"
+	"reflect"
 	"testing"
 	"time"
 
@@ -58,4 +59,104 @@ func TestBoltStore_AddBook(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, testBookID, book.ID)
 	assert.Equal(t, "Bolt test book title", book.Title)
+}
+
+// Ensure bolt store returns exact book details if exist.
+func TestBoltStore_GetOneBook_FoundBook(t *testing.T) {
+	bs, err := newTestBoltStore()
+	require.NoError(t, err, "failed in creating a test bolt store")
+	defer bs.closeTestBoltStore()
+	testBookID := "b:0"
+
+	// Create a new book.
+	b := Book{
+		ID:          testBookID,
+		Title:       "Bolt test book title",
+		Description: "Bolt test book desc",
+		Author:      "Jerome Amon",
+		Price:       "10$",
+		CreatedAt:   "2023-04-26 21:42:10.7604632 +0000 UTC",
+		UpdatedAt:   "2023-04-26 21:42:10.7604632 +0000 UTC",
+	}
+	err = bs.Add(context.TODO(), testBookID, b)
+	assert.NoError(t, err)
+
+	// Verify book does exist.
+	book, err := bs.GetOne(context.TODO(), testBookID)
+	assert.NoError(t, err)
+	if !reflect.DeepEqual(book, b) {
+		t.Errorf("Got %v but Expected %v.", book, b)
+	}
+}
+
+// Ensure bolt store returns an error if book does not exist.
+func TestBoltStore_GetOneBook_ErrNotFoundBook(t *testing.T) {
+	bs, err := newTestBoltStore()
+	require.NoError(t, err, "failed in creating a test bolt store")
+	defer bs.closeTestBoltStore()
+	testBookID := "b:0"
+
+	// Create a new book.
+	b := Book{ID: testBookID, Title: "Bolt test book title"}
+	err = bs.Add(context.TODO(), testBookID, b)
+	assert.NoError(t, err)
+
+	// Verify another book does not exist.
+	book, err := bs.GetOne(context.TODO(), "b:1")
+	assert.Equal(t, ErrNotFoundBook, err)
+	assert.Equal(t, Book{}, book)
+}
+
+// Ensure bolt store can remove a book.
+func TestBoltStore_DeleteBook(t *testing.T) {
+	bs, err := newTestBoltStore()
+	require.NoError(t, err, "failed in creating a test bolt store")
+	defer bs.closeTestBoltStore()
+	testBookID := "b:0"
+
+	// Create a new book.
+	b := Book{
+		ID:          testBookID,
+		Title:       "Bolt test book title",
+		Description: "Bolt test book desc",
+		Author:      "Jerome Amon",
+		Price:       "10$",
+		CreatedAt:   "2023-04-26 21:42:10.7604632 +0000 UTC",
+		UpdatedAt:   "2023-04-26 21:42:10.7604632 +0000 UTC",
+	}
+	err = bs.Add(context.TODO(), testBookID, b)
+	assert.NoError(t, err)
+
+	// Delete the book.
+	err = bs.Delete(context.TODO(), testBookID)
+	assert.NoError(t, err)
+
+	// Verify book does not exist.
+	book, err := bs.GetOne(context.TODO(), testBookID)
+	assert.Equal(t, ErrNotFoundBook, err)
+	assert.Equal(t, Book{}, book)
+}
+
+// Ensure bolt store can multiple books.
+func TestBoltStore_GetAllBooks(t *testing.T) {
+	bs, err := newTestBoltStore()
+	require.NoError(t, err, "failed in creating a test bolt store")
+	defer bs.closeTestBoltStore()
+	testBook0ID := "b:0"
+	testBook1ID := "b:1"
+
+	// Create some new books.
+	b0 := Book{ID: testBook0ID, Title: "Bolt test book 0 title"}
+	err = bs.Add(context.TODO(), testBook0ID, b0)
+	assert.NoError(t, err)
+	b1 := Book{ID: testBook1ID, Title: "Bolt test book 1 title"}
+	err = bs.Add(context.TODO(), testBook1ID, b1)
+	assert.NoError(t, err)
+
+	// Verify books can be retrieved.
+	books, err := bs.GetAll(context.TODO())
+	assert.NoError(t, err)
+	if !reflect.DeepEqual(books, []Book{b0, b1}) {
+		t.Errorf("Got %v but Expected %v.", books, []Book{b0, b1})
+	}
 }
