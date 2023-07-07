dfa2ee717575a648cdf78582582291afe94bc9df
Jerome Amon <aaboujerome@gmail.com>
2023-07-07 22:28:51 +0000

    feat: add interface for uid generator

--- helpers.handlers.go
+++ helpers.handlers.go
@@ -1,13 +1,24 @@
 package main
 
-import "time"
+import (
+	"time"
 
-// ensure Clock implements Clocker.
-var _ Clocker = (*Clock)(nil)
+	"github.com/gofrs/uuid"
+)
+
+var (
+	_ Clocker      = (*Clock)(nil)             // ensure Clock implements Clocker.
+	_ UIDGenerator = (*ObjectIDGenerator)(nil) // ensure ObjectIDGenerator implements UIDGenerator.
+)
 
 // Clocker is an interface for getting current real time.
 type Clocker interface {
 	Now() time.Time
+}
+
+// UIDGenerator is an interface for getting a uid.
+type UIDGenerator interface {
+	Generate(prefix string) string
 }
 
 // Clock implements the Clocker interface.
@@ -21,4 +32,18 @@ func NewClock() *Clock {
 // Now provides current clock time.
 func (ck *Clock) Now() time.Time {
 	return time.Now()
+}
+
+// ObjectIDGenerator implements the UIDGenerator interface.
+type ObjectIDGenerator struct{}
+
+// NewObjectIDGenerator returns a ready to use ObjectIDGenerator.
+func NewObjectIDGenerator() *ObjectIDGenerator {
+	return &ObjectIDGenerator{}
+}
+
+// Generate provides a random unique identifier.
+func (g *ObjectIDGenerator) Generate(prefix string) string {
+	id, _ := uuid.NewV4()
+	return prefix + ":" + id.String()
 }
