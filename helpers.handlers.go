cb8fa8cc6fe62d7a4b553b23cc4ee07f43fbd908
Jerome Amon <aaboujerome@gmail.com>
2023-07-09 12:05:00 +0000

    refactor: update uid generator interface and its implementation

--- helpers.handlers.go
+++ helpers.handlers.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"strings"
 	"time"
 
 	"github.com/gofrs/uuid"
 )
 
 var (
-	_ Clocker      = (*Clock)(nil)             // ensure Clock implements Clocker.
-	_ UIDGenerator = (*ObjectIDGenerator)(nil) // ensure ObjectIDGenerator implements UIDGenerator.
+	_ Clocker    = (*Clock)(nil)      // ensure Clock implements Clocker.
+	_ UIDHandler = (*IDsHandler)(nil) // ensure IDsHandler implements UIDHandler.
 )
 
 // Clocker is an interface for getting current real time.
@@ -17,8 +18,9 @@ type Clocker interface {
 }
 
 // UIDGenerator is an interface for getting a uid.
-type UIDGenerator interface {
+type UIDHandler interface {
 	Generate(prefix string) string
+	IsValid(prefix string, id string) bool
 }
 
 // Clock implements the Clocker interface.
@@ -34,16 +36,24 @@ func (ck *Clock) Now() time.Time {
 	return time.Now()
 }
 
-// ObjectIDGenerator implements the UIDGenerator interface.
-type ObjectIDGenerator struct{}
+// IDsHandler implements the UIDHandler interface.
+type IDsHandler struct{}
 
-// NewObjectIDGenerator returns a ready to use ObjectIDGenerator.
-func NewObjectIDGenerator() *ObjectIDGenerator {
-	return &ObjectIDGenerator{}
+// NewIDsHandler returns a ready to use IDsHandler.
+func NewIDsHandler() *IDsHandler {
+	return &IDsHandler{}
 }
 
 // Generate provides a random unique identifier.
-func (g *ObjectIDGenerator) Generate(prefix string) string {
+func (idh *IDsHandler) Generate(prefix string) string {
 	id, _ := uuid.NewV4()
 	return prefix + ":" + id.String()
+}
+
+// IsValid checks if a given string is a valid uuid after removal of custom prefix.
+func (idh *IDsHandler) IsValid(id, prefix string) bool {
+	if u := uuid.FromStringOrNil(strings.TrimPrefix(id, prefix+":")); u != uuid.Nil {
+		return true
+	}
+	return false
 }
