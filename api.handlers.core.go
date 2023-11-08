8e3e7ee26b9a54d568e17811fab7a372809fccc5
Jerome Amon <aaboujerome@gmail.com>
2023-11-09 00:59:57 +0100

    feat: add custom notfound handler which produces meaningful json response

--- api.handlers.core.go
+++ api.handlers.core.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
 	"sync"
 
 	"go.uber.org/zap"
@@ -24,4 +26,31 @@ func NewAPIHandler(logger *zap.Logger, config *Config, stats *Statistics, ck Clo
 	stats.status = make(map[int]uint64)
 	stats.mu = &sync.RWMutex{}
 	return &APIHandler{logger: logger, config: config, stats: stats, mode: m, clock: ck, idsHandler: idsHandler, bookService: bs}
+}
+
+// NotFound is a custom handler used to serve inexistant requested routes.
+func (api *APIHandler) NotFound() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestID := api.idsHandler.Generate(RequestIDPrefix)
+		logger := api.logger.With(
+			zap.String("request.id", requestID),
+			zap.String("request.method", r.Method),
+			zap.String("request.path", r.URL.Path),
+			zap.String("request.ip", GetRequestSourceIP(r)),
+			zap.String("request.agent", r.UserAgent()),
+			zap.String("request.referer", r.Referer()),
+		)
+		logger.Warn("unknown route")
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusNotFound)
+		if err := json.NewEncoder(w).Encode(
+			map[string]string{
+				"requestid": requestID,
+				"message":   "route does not exist",
+				"path":      r.Method + " " + r.URL.Path,
+			},
+		); err != nil {
+			api.logger.Error("failed to send response", zap.String("request.id", requestID))
+		}
+	})
 }
--- api.router.core.go
+++ api.router.core.go
@@ -7,6 +7,7 @@ import (
 // SetupRoutes injects book and ops related endpoints if required.
 func (api *APIHandler) SetupRoutes(router *httprouter.Router, m *MiddlewareMap) *httprouter.Router {
 	router.RedirectTrailingSlash = true
+	router.NotFound = api.NotFound()
 	api.SetupBookRoutes(router, m)
 	if api.config.OpsEndpointsEnable {
 		api.SetupOpsRoutes(router, m)
